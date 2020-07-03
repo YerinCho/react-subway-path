1df76cf965793c08de7780ecc59e9c76fa0824fc
YerinCho <forjustice9839@gmail.com>
2020-07-04 03:02:49 +0900

    refactor: 반복되는 부분 InputStation 에 추가

--- src/pages/Input.js
+++ src/pages/Input.js
@@ -16,23 +16,19 @@ const Input = ({search}) => {
   return (
     <form className="bg-white mb-4">
       <div className="flex flex-wrap mb-3">
-        <div className="w-5/12 h-12 text-center text-gray-800">
           <InputStation
             station={departureStation}
             type={"출발역"}
             handleChange={handleChangeDeparture}
           />
-        </div>
         <div className="w-2/12 h-12 text-center text-gray-800 flex justify-center items-center">
           <span className="mdi mdi-arrow-right-thick relative bottom-6px text-lg"/>
         </div>
-        <div className="w-5/12 h-12 text-center text-gray-800">
           <InputStation
             station={arrivalStation}
             type={"도착역"}
             handleChange={handleChangeArrival}
           />
-        </div>
       </div>
       <button type="button" id="search-button"
               className="w-full text-sm bg-yellow-500 hover:bg-yellow-400 hover:text-gray-700 text-gray-800 font-bold py-2 px-4 rounded-sm"
--- src/pages/InputStation.js
+++ src/pages/InputStation.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const InputStation =({station, type, handleChange}) => {
   return (
+    <div className="w-5/12 h-12 text-center text-gray-800">
     <input
       className="appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
       id="departure-station-name"
@@ -10,6 +11,7 @@ const InputStation =({station, type, handleChange}) => {
       placeholder={type}
       onChange={handleChange}
     />
+    </div>
   );
 }
 export default InputStation
